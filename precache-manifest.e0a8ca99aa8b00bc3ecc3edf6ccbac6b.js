f2d839411ee296ae7a57b3b72c2426d5fce04bbd
Sergey Khalyavin <serg.khalk@gmail.com>
2020-09-19 21:30:01 +0300

    Updates

---
+++ precache-manifest.e0a8ca99aa8b00bc3ecc3edf6ccbac6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b525e43f522c5386b96f4d19f20fbc",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "06c8c75c939a05ae43fb",
+    "url": "/goit-react-hw-02-feedback/static/css/main.3ef30b17.chunk.css"
+  },
+  {
+    "revision": "cc5f388607c2a308c9ee",
+    "url": "/goit-react-hw-02-feedback/static/js/2.2f63911d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.2f63911d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c8c75c939a05ae43fb",
+    "url": "/goit-react-hw-02-feedback/static/js/main.502d81d9.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
